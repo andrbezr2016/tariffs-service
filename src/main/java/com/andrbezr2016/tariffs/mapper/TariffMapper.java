598d3ccb8719d43331093d8b5f4dc64066022937
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-21 22:00:00 +0300

    refactoring

--- src/main/java/com/andrbezr2016/tariffs/mapper/TariffMapper.java
+++ src/main/java/com/andrbezr2016/tariffs/mapper/TariffMapper.java
@@ -5,16 +5,12 @@ import com.andrbezr2016.tariffs.dto.TariffRequest;
 import com.andrbezr2016.tariffs.entity.TariffEntity;
 import org.mapstruct.Mapper;
 
-import java.util.Collection;
-
 @Mapper(componentModel = "spring")
 public interface TariffMapper {
 
     Tariff toDto(TariffEntity tariffEntity);
 
     TariffEntity toEntity(TariffRequest tariffRequest);
-
-    Collection<Tariff> toDtoCollection(Collection<TariffEntity> tariffEntityList);
 
     TariffEntity copyEntity(TariffEntity tariffEntity);
 }
